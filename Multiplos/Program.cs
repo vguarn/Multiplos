351e80e82b1f0105590bcf53729648f3e44b6a92
Usuario <Usuario@CPD02-M14>
2024-03-26 10:46:37 -0300

    Adicionar arquivos de projeto.

---
+++ Multiplos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual número você deseja ver os múltiplos?");
+            int mult = int.Parse(Console.ReadLine());
+
+            int cont = mult;
+
+            while (cont <= 100)
+            {
+                Console.WriteLine(cont);
+                cont += mult;
+            }
+             Console.ReadKey();
+        }
+    }
+}
